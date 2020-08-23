dce8321343365af179448b381698cdacf75ec29b
rpcagoco <69941216+rpcagoco@users.noreply.github.com>
2020-08-23 16:29:33 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+	
+<html>
+<body style="background-color:#29bdc1">
+<style>
+p {
+  margin: 50px;
+}
+</style>
+</head>
+<body>
+
+<h1 style="text-align:center;">Personal Introduction</h1>
+
+<p>Hello, my name is Richelle Cagoco. When it comes to introducing myself people tend to mispronounce my name which I am already used to. But you can call me <b>"Richie"</b> sfor short, one of my good friends gave me that nickname and I just went along with it.</p>
+
+<p>I don't have a much knowledge of coding as of now though I do know a HTML, Java, and still learning Python.</p>
+
+</body>
+</html>
+
+<?php include_once(“index.html") ?>
